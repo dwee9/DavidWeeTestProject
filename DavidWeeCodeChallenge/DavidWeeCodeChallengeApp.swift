5a6997d9db24fd7e46601fa86a5e12f7636d6f29
dwee9 <dgwee9@gmail.com>
2024-03-01 12:13:57 -0600

    Initial Commit

---
+++ DavidWeeCodeChallenge/DavidWeeCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DavidWeeCodeChallengeApp.swift
+//  DavidWeeCodeChallenge
+//
+//  Created by David Wee on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct DavidWeeCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
